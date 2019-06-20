299f1035411cec9f074e5d4ff47bf26c17f65a53
Ciara Gardiner <ciara.gardiner14@gmail.com>
2019-06-20 17:47:35 +0100

    Add timers and output (csv) to app

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 var shell = require('shelljs');
-shell.exec('java -jar generator.jar generate --max-rows=100 --allow-untyped-fields --replace profile.json output.csv', (code, stdout, stderr) => {
-    console.log(stdout);
-    console.log('Exit code:', code);
-}) ;
+const fs = require('fs');
+const path = require('path');
+
+const runTest = (mode) => {
+    const startTime = Date.now();
+    const result = shell.exec(`java -jar generator.jar ${mode} --max-rows=10 --allow-untyped-fields --replace profile.json output.csv`);
+    const endTime = Date.now();
+
+    var difference = (endTime - startTime)/1000;
+    console.log("Running time is: " + difference);
+    console.log(process.cwd());
+    fs.writeFile(path.join(".", "results", `${Date.now()}.csv`), difference, function(err) {
+        if(err) {
+            return console.log(err);
+        }
+    
+        console.log("The file was saved!");
+    });
+}
+
+runTest('generate');
+//runTest('generate --violate');
