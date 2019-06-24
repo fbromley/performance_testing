436cfefa41b2cc3927a104481da4936ba386d9f2
Frances Bromley <fbromley@scottlogic.com>
2019-06-24 20:33:16 +0100

    remove build from inside test run

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ var shell = require('shelljs');
 const fs = require('fs');
 const path = require('path');
 
+const buildDataHelix = () => {
+    const Img = shell.exec('docker build . -t generator');
+}
 
 const runTest = (mode) => {
-    const Img = shell.exec('docker build . -t generator');
     const startTime = Date.now();
     const result = shell.exec(`docker run --rm generator ${mode} --max-rows=100 --allow-untyped-fields --replace --profile-file=profile.json --output-path=output.csv`);
     const endTime = Date.now();
@@ -21,5 +23,6 @@ const runTest = (mode) => {
     });
 }
 
+buildDataHelix();
 runTest("generate");
 runTest("violate");
